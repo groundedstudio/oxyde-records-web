86cca5effd18965eb1d459930e65ff6a26d733a3
Sam Drew <sam-drew@outlook.com>
2020-06-09 14:31:25 +0100

    :tada: Framework for Oxyde's webiste

---
+++ js/logo-displace.js
@@ -0,0 +1,34 @@
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+const canvas = document.getElementById('logo')
+const app = new PIXI.Application({width: 2010, height: 432, transparent: true, resolution: 1, view: canvas});
+
+app.stage.interactive = true;
+
+const container = new PIXI.Container();
+app.stage.addChild(container);
+
+const logo = PIXI.Sprite.from('../static/logo.png');
+container.addChild(logo);
+logo.x = 0;
+logo.y = 0;
+logo.height = 427;
+logo.width = 2000;
+
+
+const displacementSprite = PIXI.Sprite.from('../static/clouds.jpg');
+// Make sure the sprite is wrapping.
+displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+const displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+displacementSprite.position = logo.position;
+
+app.stage.addChild(displacementSprite);
+app.stage.filters = [displacementFilter]
+
+animate();
+
+function animate() {
+    displacementSprite.x += 10;
+    displacementSprite.y += 4;
+    requestAnimationFrame(animate);
+}
